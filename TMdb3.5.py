e697a17c14a273d482cf6fc4ffe6cfd20a7c0a9e
Abhishek Shrivastava <shriabhishek929@gmail.com>
2023-06-03 23:12:43 +0530

    Create TMdb3.5.py

---
+++ TMdb3.5.py
@@ -0,0 +1,14 @@
+# import requests
+# ## Write your code here
+# apiKey = "2b771a8d433185f787240fc50f7d077d"
+# res = requests.get(F'https://api.themoviedb.org/3/tv/{ID}/credits',params={'api_key':apiKey}) 
+# data=res.json()
+# Female=0
+# Male=0
+# for  i in data['cast']:
+#     if i['gender']==1:
+#         Male+=1
+#     elif i['gender']==2:
+#         Female+=1
+
+print("10  5")
